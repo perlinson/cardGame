4b2578620ed1bd90d0a1c9918bffc196ab5ad36f
perlinson <perlinson_2008@163.com>
2021-10-17 19:05:46 +0800

    初次提交

--- front/components/Home/index.vue
+++ front/components/Home/index.vue
@@ -4,6 +4,30 @@
   </div>
 </template>
 <script>
-export default {}
+export default {
+  mounted() {
+    this.socket = this.$nuxtSocket({
+      channel: '/index',
+    })
+    /* Listen for events: */
+    this.socket.on('someEvent', (msg, cb) => {
+      /* Handle event */
+    })
+  },
+  methods: {
+    method1() {
+      /* Emit events */
+      this.socket.emit(
+        'method1',
+        {
+          hello: 'world',
+        },
+        (resp) => {
+          /* Handle response, if any */
+        }
+      )
+    },
+  },
+}
 </script>
 <style></style>
